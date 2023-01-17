ef0d05f8adfbaaa31e119298de680dc48642eddb
kcjesse02 <william.vanuitert@gmail.com>
2023-01-16 20:00:09 -0500

    Create DriveToPoint.java

---
+++ src/main/java/frc/robot/commands/DriveToPoint.java
@@ -0,0 +1,83 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import java.sql.Savepoint;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+
+public class DriveToPoint extends CommandBase {
+  /** Creates a new DriveToPoint. */
+  
+  double distanceTolerance = 0.1;
+  double distance;
+  double velocity;
+
+  double alpha = 1;
+  double beta = 1.5;
+  double gamma = 1.5;
+
+  Pose2d robotPose;
+  Pose2d targetPose;
+  DriveSubsystem drivetrain;
+  ChassisSpeeds chassisSpeeds;
+  private final static double maximumLinearVelocity = 3.5;   // Meters/second
+  private final static double maximumRotationVelocity = 4.0; // Radians/second
+
+  public DriveToPoint(DriveSubsystem ds, Pose2d targetPose2d, double vroom) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = ds;
+    this.targetPose = targetPose2d;
+    this.velocity = vroom;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    // Called every time the scheduler runs while the command is scheduled.
+    distance = 0;
+    double targetAngle = targetPose.getRotation().getRadians();
+
+ }
+
+
+  @Override
+  public void execute() {
+    //getOdometry not complete
+    robotPose = drivetrain.getOdometry();
+    var error = targetPose.minus(robotPose); 
+    double xVelocity = 0; 
+    double yVelocity = 0;
+    double angularVelocity = 0; 
+    double velocityMult = maximumLinearVelocity;
+    double rotateMult = maximumRotationVelocity;
+    
+    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+        yVelocity * velocityMult,
+        xVelocity * velocityMult, 
+        angularVelocity * rotateMult,
+        Rotation2d.fromDegrees(drivetrain.getHeading())); // get fused heading
+    drivetrain.setChassisSpeeds(speeds);
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    var error = targetPose.minus(robotPose);
+    if (error.getTranslation().getNorm()< 0.1 && error.getRotation().getRadians()< 0.05) {
+      return true;
+    }
+    return false;
+  }
+}
