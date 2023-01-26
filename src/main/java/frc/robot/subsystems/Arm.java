3aad4ba8ba671abf16c9e17a804ec96888555b59
TomP6 <thomas.p@frc1073.org>
2023-01-25 19:42:31 -0500

    Added arm and claw subsystems

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+  public class JointPositions{
+    double shoulder;
+    double elbow;
+  }
+
+  public class JointVelocities{
+    double shoulder;
+    double elbow;
+  }
+
+  public class CartesianPosition{
+    double x;
+    double z;
+    double pitch;
+  }
+
+  /** Creates a new Arm. */
+  //Set height limiter
+  public Arm() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  // This methods returns the angle of each joint
+  public JointPositions getJointAngles(){
+    return null;
+  }
+
+  // This method returns the maximum angles of joints
+  public JointPositions getMaxAngles(){
+    return null;
+  }
+
+  // This method returns the minimum angles of joints
+  public JointPositions getMinAngles(){
+    return null;
+  }
+
+  // This method sets a target angle for joints
+  public void setTargetAngle(JointPositions target){
+
+  }
+
+  // This method sets a target speed for joints
+  public void setJointVelocities(JointVelocities speed){
+
+  }
+
+  // This method returns the position of clawgiven different joint angles
+  public CartesianPosition getCartesianPosition(JointPositions positions){
+    return null;
+  }
+
+  // This method returns the joint angles given a position of the claw
+  public JointPositions getInverseKinematics(CartesianPosition clawPose){
+    return null;
+  }
+}
