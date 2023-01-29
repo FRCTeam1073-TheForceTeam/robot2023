3f9abf49dfd7a788296e416261df35bf2f27c5f0
Alex <alex.h@frc1073.org>
2023-01-29 10:40:37 -0800

    Updating Branch
    Please don't let your branch get 21 commits behind main, y'all.

---
+++ src/main/java/frc/robot/subsystems/AprilTagDetection.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+package edu.wpi.first.apriltag;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.numbers.N3;
+import java.util.Arrays;
+
+public class AprilTagDetection extends SubsystemBase {
+  /** Creates a new AprilTag. */
+  public AprilTagDetection() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  // Initialize preferences for this class:
+  public static void initPreferences() {
+  
+  }
+
+  // This method returns the position of the robot based on information from AprilTags
+  public Pose2d getFieldPose(){
+    return new Pose2d();
+  }
+
+  // This method returns the position of the passed in AprilTag ID relative to the Robot
+  public Pose3d getRelativePose(int id){
+    //getTagPose method in AprilTagFieldLayout
+    return new Pose3d();
+  }
+
+  //This method returns an array of all the visible AprilTag IDs
+  public ArrayList<AprilTag> getVisibleTags()
+  {
+    return null;
+  }
+
+}
