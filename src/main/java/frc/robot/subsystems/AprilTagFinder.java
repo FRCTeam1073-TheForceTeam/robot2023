805c01b11788ec02153914ac73761e62192ba5f6
I v a n <107157923+C0leSc0ttB0t@users.noreply.github.com>
2023-01-30 19:42:01 -0500

    don't go deleting my code
    (I won't go deleting your code)
    
    Co-Authored-By: alex <alex.h@frc1073.org>

--- src/main/java/frc/robot/subsystems/AprilTagFinder.java
+++ src/main/java/frc/robot/subsystems/AprilTagFinder.java
@@ -46,48 +46,5 @@ public class AprilTagFinder extends SubsystemBase {
   {
     return null;
   }
-  
-   /**
-   * Gets the decoded ID of the tag.
-   *
-   * @return Decoded ID
-   */
-
-  public int getId() {
-    return m_id;
-  }
-
-  /**
-   * Gets how many error bits were corrected. Note: accepting large numbers of corrected errors
-   * leads to greatly increased false positive rates. NOTE: As of this implementation, the detector
-   * cannot detect tags with a hamming distance greater than 2.
-   *
-   * @return Hamming distance (number of corrected error bits)
-   */
-  public int getHamming() {
-    return m_hamming;
-  }
-
-  /**
-   * Gets a measure of the quality of the binary decoding process: the average difference between
-   * the intensity of a data bit versus the decision threshold. Higher numbers roughly indicate
-   * better decodes. This is a reasonable measure of detection accuracy only for very small tags--
-   * not effective for larger tags (where we could have sampled anywhere within a bit cell and still
-   * gotten a good detection.)
-   *
-   * @return Decision margin
-   */
-  public float getDecisionMargin() {
-    return m_decisionMargin;
-  }
-
-  /**
-   * Gets the 3x3 homography matrix describing the projection from an "ideal" tag (with corners at
-   * (-1,1), (1,1), (1,-1), and (-1, -1)) to pixels in the image.
-   *
-   * @return Homography matrix data
-   */
-  @SuppressWarnings("PMD.MethodReturnsInternalArray")
-  public double[] getHomography() {
-    return m_homography;
 }
+
