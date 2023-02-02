70cd776cb3b402e335a3faf7e246fec24a7cf3b7
Oli <24oliviao@gmail.com>
2023-02-01 20:35:16 -0500

    Added skeleton commands for arm

---
+++ src/main/java/frc/robot/commands/DebugArm.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Arm;
+
+public class DebugArm extends CommandBase {
+  private Arm arm;
+
+  /** Creates a new DebugArm. */
+  public DebugArm() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(arm);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(false){
+      arm.setTargetAngle(new Arm.JointPositions(0,0)); //tuck arm in
+    }
+
+    if(false){
+      //move shoulder
+    }
+
+    if(false){
+      //move elbow
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
