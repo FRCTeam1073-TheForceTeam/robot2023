656989be731701881cc96d81b0f63257af68cf33
kcjesse02 <william.vanuitert@gmail.com>
2023-02-02 18:45:43 -0500

    Fixed Energized and put in indents

--- src/main/java/frc/robot/commands/Energize.java
+++ src/main/java/frc/robot/commands/Energize.java
@@ -7,13 +7,14 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class Energize extends CommandBase {
   /** Creates a new Energize. */
 
-  int drivePhase; //start, drive, climb stop
+  int drivePhase; //start, drive, climb, stop
   double distanceTolerance = 0.1;
   double angleTolerance = 0.05;
   boolean forward; //camera faces april tag
@@ -35,74 +36,53 @@ public class Energize extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drivePhase = 0;
+    drivePhase = 0; //starts  moving
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  if (drivePhase == 0) {
-    robotPose = drivetrain.getOdometry();
-    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-        maxLinearVelocity,
-        0, 
-        0,
-        Rotation2d.fromDegrees(drivetrain.getHeading())); // get fused heading
-      drivetrain.setChassisSpeeds(speeds);
-    //double yVelocity = - 0.8 * difference.getY();
-    
-    if (drivetrain.getPitch() > 10) {
-      drivePhase = 1;
-    }
-  } //end of phase 0
-  if (drivePhase == 1) {
-    robotPose = drivetrain.getOdometry();
-    ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-        maxLinearVelocity,
-        0, 
-        0,
-        Rotation2d.fromDegrees(drivetrain.getHeading())); // get fused heading
-      drivetrain.setChassisSpeeds(speeds);
-      System.out.println("DrivePhase 1");
-    //double yVelocity = - 0.8 * difference.getY();
+    SmartDashboard.putNumber("Drive Phase", drivePhase);
+    if (drivePhase == 0) {
+      robotPose = drivetrain.getOdometry();
+      chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+          maxLinearVelocity,
+          0, 
+          0,
+          Rotation2d.fromDegrees(drivetrain.getHeading())); // get fused heading
+        drivetrain.setChassisSpeeds(chassisSpeeds);
+      
+      if (drivetrain.getPitch() > 13) {
+        drivePhase = 1; //starts climbing
+      }
+    } //end of phase 0
 
-    if (drivetrain.getPitch() < 15 && drivePhase == 1) {
-      drivePhase = 2;
-      ChassisSpeeds speeds2 = new ChassisSpeeds(-maxLinearVelocity * 0.5, 0, 0); // go backwards slightly
-      drivetrain.setChassisSpeeds(speeds2);
-    }
-    
-    if (drivePhase == 2)
+    if (drivePhase == 1)
     {
-      //ChassisSpeeds speeds3 = new ChassisSpeeds(0, 0, 0);
-      //drivetrain.setChassisSpeeds(speeds3);
-      if (Math.abs(drivetrain.getPitch()) < 6)
-      {
-        System.out.println("DrivePhase 2");
-        drivePhase = 3;
+      
+      chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(-maxLinearVelocity * 0.5,0,0, Rotation2d.fromDegrees(drivetrain.getHeading()));
+
+      if (Math.abs(drivetrain.getPitch()) < 6) {
+        drivePhase = 2;
       }
     }
   }
 
-  }
-
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(0,0,0, Rotation2d.fromDegrees((drivetrain.getHeading())));
+    drivetrain.parkingBrake();
+  }
 
   // Returns true when the command should end.
 
   @Override
   public boolean isFinished() {
-    if (drivePhase == 3) 
+    if (drivePhase == 2) 
     {
-      System.out.println("DrivePhase 3");
-      drivetrain.parkingBrake();
-
       return true;
     }
-    else {
-      return false;
-    }
+    return false;
   }
 }
