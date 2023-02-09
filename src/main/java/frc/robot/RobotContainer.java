f0e219f3103586cca33e7edd9334f17ae46b45d0
I v a n <isgn2031@gmail.com>
2023-02-08 20:45:42 -0500

    Worked on Engage
    should be better now, but still doesn't completely work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -78,14 +78,14 @@ public class RobotContainer {
   }*/
 
   //test making last y negative and see results
-    ArrayList<Pose2d> waypoints = new ArrayList<Pose2d>();
-      waypoints.add(new Pose2d(1.0, 0.0, new Rotation2d()));
-      waypoints.add(new Pose2d(1.0, 1.0, new Rotation2d()));
-      waypoints.add(new Pose2d(2.0, 2.0, new Rotation2d(3)));
-    return new SequentialCommandGroup(
-      new DriveThroughTrajectory(m_driveSubsystem, new Pose2d(0,0, new Rotation2d()), waypoints, 0.5, 
-      0.8, 0.5, 0.5)
-    );
-    //return new SequentialCommandGroup(new Engage(m_driveSubsystem, 0.3));
+    // ArrayList<Pose2d> waypoints = new ArrayList<Pose2d>();
+    //   waypoints.add(new Pose2d(1.0, 0.0, new Rotation2d()));
+    //   waypoints.add(new Pose2d(1.0, 1.0, new Rotation2d()));
+    //   waypoints.add(new Pose2d(2.0, 2.0, new Rotation2d(3)));
+    // return new SequentialCommandGroup(
+    //   new DriveThroughTrajectory(m_driveSubsystem, new Pose2d(0,0, new Rotation2d()), waypoints, 0.5, 
+    //   0.8, 0.5, 0.5)
+    // );
+    return new SequentialCommandGroup(new Engage(m_driveSubsystem, 0.3));
   }
 }
