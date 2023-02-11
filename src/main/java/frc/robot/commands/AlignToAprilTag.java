becdf91119156b48dc2e1c7e385f13cdf0d2e363
Cole Scott <cole.scott9964@gmail.com>
2023-02-11 14:18:16 -0500

    Unfinished apriltag aline

--- src/main/java/frc/robot/commands/AlignToAprilTag.java
+++ src/main/java/frc/robot/commands/AlignToAprilTag.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import java.util.ArrayList;
 
 import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -30,9 +31,18 @@ public class AlignToAprilTag extends CommandBase {
   double maxVelocity;
   AprilTagFinder finder;
   double tolerance;
-  Pose3d difference;
+ // Pose3d difference;
   private NetworkTable apriltagConnect;
-  private NetworkTableEntry apriltagConnectEntry;
+  private NetworkTableEntry apriltagConnectEntryID;
+  private NetworkTableEntry apriltagConnectEntryY;
+  private NetworkTableEntry apriltagConnectEntryYaw;
+  int targetTagID;
+  int targetTagY;
+  int robotYaw;
+  double maxAngularSpeed;
+  double maxSpeed;
+  int phase = 0;
+  double initialHeading;
 
   //double maxRotationlVelocity;
 
@@ -42,41 +52,56 @@ public class AlignToAprilTag extends CommandBase {
     this.drivetrain = drivetrain;
     this.maxVelocity = maxVelocity;
     finder = new AprilTagFinder(drivetrain);
-    apriltagConnect = NetworkTableInstance.getDefault().getTable("Vision");
-    apriltagConnectEntry = apriltagConnect.getEntry("Closest ID");
+    maxAngularSpeed = 0.5;
+  }
 
  //   for(int i = 0; i < finder.getVisibleTags().size(); i++){
  //     visibleTagIDs.add(finder.getVisibleTags().get(i).getId());
  //   }
  //   addRequirements(drivetrain);
 
-  }
+  
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    apriltagConnect = NetworkTableInstance.getDefault().getTable("Vision");
+    apriltagConnectEntryID = apriltagConnect.getEntry("Closest ID");
+    apriltagConnectEntryY = apriltagConnect.getEntry("Closest Y");
+    apriltagConnectEntryY = apriltagConnect.getEntry("Tag Rotation Y");
+    robotYaw = (int)apriltagConnectEntryYaw.getDouble(0.0);
+    targetTagID = (int)apriltagConnectEntryID.getDouble(0.0);
+    targetTagY = (int)apriltagConnectEntryY.getDouble(0.0);
+    initialHeading = drivetrain.getHeading();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(visibleTagIDs.contains(id)){
-      difference = finder.getRelativePose(id);
-      double yVelocity = 0.5 * difference.getY();
-      if(yVelocity > maxVelocity){
-        yVelocity = maxVelocity;
-      }
-      if(yVelocity < -maxVelocity){
-        yVelocity = -maxVelocity;
+    double difference;
+    if (phase == 0){
+      if (drivetrain.getHeading() > 0){
+        difference = 180 - drivetrain.getHeading();
+      }else{
+        difference = -180 - drivetrain.getHeading();
       }
 
-      chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(0, yVelocity, 0, 
-      Rotation2d.fromDegrees(drivetrain.getHeading()));
+      if (Math.abs(difference) > .1){
+        double angularVelocity = - 0.8 * difference;
+        angularVelocity = MathUtil.clamp(angularVelocity, -maxAngularSpeed, maxAngularSpeed);
+
+      }else{
+        phase = 1;
+      }
+    }
+    
+    if (phase == 1){
+      if (initialHeading < 0){
+        //ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds( -xSpeed, Rotation2d.fromDegrees(drivetrain.getHeading()));
+      }
     }
   
-    else{
-      chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, 0, 
-      Rotation2d.fromDegrees(drivetrain.getHeading()));
-    }
+    
   }
 
   // Called once the command ends or is interrupted.
@@ -90,9 +115,9 @@ public class AlignToAprilTag extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(difference.getY() < tolerance){
-      return true;
-    }
+    //if(difference.getY() < tolerance){
+      //return true;
+    //}
     return false;
   }
 }
