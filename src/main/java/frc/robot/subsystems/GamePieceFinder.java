9bea1d08807ec4b9281949a02e135ec8204af14f
Cole Scott <cole.scott9964@gmail.com>
2023-03-12 11:32:48 -0400

    (NOT TESTED) (CUBES ONLY) Game Piece Finder subsystem.
    Added SmartDashboard widgets, and got the x, y, width, and height of the game piece. (CUBES ONLY).

--- src/main/java/frc/robot/subsystems/GamePieceFinder.java
+++ src/main/java/frc/robot/subsystems/GamePieceFinder.java
@@ -5,6 +5,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -13,23 +16,88 @@ import java.util.ArrayList;
 
 public class GamePieceFinder extends SubsystemBase {
   /** Creates a new GamePieceFinder. */
+  public class GamePiece {
+    public double x = 0.0;
+    public double y = 0.0;
+    public double w = 0.0;
+    public double h = 0.0;
+  }
+
   private NetworkTable gamePieceNetwork;
   private NetworkTableEntry gamePieceEntry;
   private String tableName;
   private String closestGamePiece;
   private double closestDistance;
   private DriveSubsystem driveSubsystem;
-  public GamePieceFinder(DriveSubsystem ds, String tableName) {
+  private ArrayList<GamePiece> gamePieceArray;
+  private Transform3d cameraTransform;
+  private GamePiece gamePiece;
+
+  public GamePieceFinder(DriveSubsystem ds, String tableName, Transform3d cameraTransform3d) {
     driveSubsystem = ds;
     this.tableName = tableName;
+    this.cameraTransform = cameraTransform;
     gamePieceNetwork = NetworkTableInstance.getDefault().getTable(tableName);
     gamePieceEntry = gamePieceNetwork.getEntry("Cubes");
+    gamePieceArray = new ArrayList<GamePiece>();
+    
     closestGamePiece = "None";
     closestDistance = 99999.0;
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    Number[] gamePieceData = gamePieceEntry.getNumberArray(new Number[0]);
+    gamePieceArray.clear();
+    int numGamePiece = gamePieceData.length/4;
+    // Reset search variables for clostest to empty:
+    closestGamePiece = "None";
+    closestDistance = 9999.0;
+
+
+    for (int i = 0; i < numGamePiece; i = i +1){
+      
+      double[] cubes = new double[3];
+
+      cubes[0] = gamePieceData[i*4 + 1].doubleValue();
+      cubes[1] = gamePieceData[i*4 + 2].doubleValue();
+      cubes[2] = gamePieceData[i*4 + 3].doubleValue();
+      cubes[3] = gamePieceData[i*4 + 4].doubleValue();
+
+      gamePiece.x = cubes[0];
+      gamePiece.y = cubes[1];
+      gamePiece.w = cubes[2];
+      gamePiece.h = cubes[3];
+
+
+    }
+
+
+    SmartDashboard.putNumber(String.format("%s/NumGamePieces", tableName), numGamePiece);
+    if (closestGamePiece != "None") {
+      SmartDashboard.putString(String.format("%s/ClosestGamePiece", tableName), closestGamePiece);
+      SmartDashboard.putNumber(String.format("%s/ClosestDistance", tableName), closestDistance);
+      SmartDashboard.putNumber(String.format("%s/GamePieceX", tableName), gamePiece.x);
+      SmartDashboard.putNumber(String.format("%s/GamePieceY", tableName), gamePiece.y);
+      SmartDashboard.putNumber(String.format("%s/GamePieceWidth", tableName), gamePiece.w);
+      SmartDashboard.putNumber(String.format("%s/GamePieceHeight", tableName), gamePiece.h);
+    }else{
+      SmartDashboard.putString(String.format("%s/ClosestGamePiece", tableName), "None");
+      SmartDashboard.putNumber(String.format("%s/ClostestDistance", tableName), 99999.0);
+      SmartDashboard.putNumber(String.format("%s/GamePieceX", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/GamePieceY", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/GamePieceWidth", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/GamePieceHeight", tableName), 0.0);
+    }
+  }
+
+
+  public Pose2d getFieldPose() {
+    if (gamePieceArray.size() > 0) {
+      //TODO: Implement this method when needed
+      return null; // Until we implement it.
+    }else{
+      return null;
+    }
   }
 }
