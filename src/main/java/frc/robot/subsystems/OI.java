128fad3cd2d78a1a92d23aa4b8ca3cf42dc73c70
TomP6 <thomas.p@frc1073.org>
2023-03-16 21:16:45 -0400

    drive ccontrols

--- src/main/java/frc/robot/subsystems/OI.java
+++ src/main/java/frc/robot/subsystems/OI.java
@@ -16,7 +16,7 @@ public class OI extends SubsystemBase
 
     public Joystick driverController;
     public Joystick operatorController;
-    Debouncer parkingBrakeDebouncer = new Debouncer(0.1);
+    Debouncer parkingBrakeDebouncer = new Debouncer(0.05);
     //private BooleanSupplier isCube;
     private boolean isCubeMode;
 
