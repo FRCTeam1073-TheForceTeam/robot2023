9bee03f1ee0ffa75c38da708b8f78e10216b1a51
Cole Scott <107157923+C0leSc0ttB0t@users.noreply.github.com>
2023-03-25 12:01:40 -0400

    Added GamePieceFinder and AlignToGamePiece
    Co-Authored-By: Mindi Hu <92765324+Mindi088@users.noreply.github.com>
    Co-Authored-By: C0leSc0ttB0t <107157923+C0leSc0ttB0t@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/AlignToGamePiece.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Bling;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.GamePieceFinder;
+
+public class AlignToGamePiece extends CommandBase {
+  /** Creates a new AlignToGamePiece. */
+  private DriveSubsystem drivetrain;
+  private Bling bling;
+  private GamePieceFinder finder;
+  private double maxVelocity;
+  private double maxAnguarVelocity;
+  private double tolerance;
+
+
+  // State variables for execution:
+  String targetGamePiece;
+  double targetGamePieceDistance;
+  private ChassisSpeeds chassisSpeeds;
+  int glitchCounter;
+  double yOffset;
+
+
+  public AlignToGamePiece(DriveSubsystem drivetrain, Bling bling, GamePieceFinder finder, double maxVelosity) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = drivetrain;
+    this.bling = bling;
+    this.finder = finder;
+    this.maxAnguarVelocity = 0.2;
+    this.tolerance = 0.05;
+    this.chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    String closestGamePiece = finder.getClosestGamePiece();
+    bling.clearLEDs();
+    if(closestGamePiece != "None"){
+      System.out.println(String.format("AlignToGamePiece Initialized to %s", closestGamePiece));
+      targetGamePiece = closestGamePiece;
+    } 
+    else {
+      targetGamePiece = "None";
+      System.out.println("AlignToGamePiece Initialize Failed: No Game Piece in Signt!");
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    bling.setColorRGBAll(255, 255, 255);
+    String closestGamePiece = finder.getClosestGamePiece();
+   // TODO:Pose3d targetPose = finder.getClosestPose();
+   double currentHeading = drivetrain.getWrappedHeading();
+
+   //TODO: if(targetPose != null){
+     //targetPose = new Pose3d(new Translation3d(targetPose.getX(), targetPose.getY(), targetPose.getZ()), targetPose.getRotation());
+  // }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/GamePieceFinder.java
+++ src/main/java/frc/robot/subsystems/GamePieceFinder.java
@@ -4,16 +4,175 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.ArrayList;
 
-/** Add your docs here. */
-public class GamePieceFinder extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+public class GamePieceFinder extends SubsystemBase {
+  /** Creates a new GamePieceFinder. */
+  public class GamePiece {
+    public double x = 0.0;
+    public double y = 0.0;
+    public double w = 0.0;
+    public double h = 0.0;
+  }
+
+  private NetworkTable cubeNetwork;
+  private NetworkTable coneNetwork;
+  private NetworkTableEntry cubePieceEntry;
+  private NetworkTableEntry conePieceEntry;
+  private String tableName;
+  private String closestCube;
+  private double closestCubeDistance;
+  private String closestCone;
+  private double closestConeDistance;
+  private DriveSubsystem driveSubsystem;
+  private ArrayList<GamePiece> cubeArray;
+  private ArrayList<GamePiece> coneArray;
+  private Transform3d cameraTransform;
+  private GamePiece cube;
+  private GamePiece cone;
+  private GamePiecePoseEstimator poseEstimator;
+
+  public class GamePiecePoseEstimator {
+    //public Transform3d estimate(){
+    //}
+  }
+
+  public GamePieceFinder(DriveSubsystem ds, String tableName, Transform3d cameraTransform3d) {
+    driveSubsystem = ds;
+    this.tableName = tableName;
+    cubeNetwork = NetworkTableInstance.getDefault().getTable(tableName);
+    cubePieceEntry = cubeNetwork.getEntry("Cubes");
+    cubeArray = new ArrayList<GamePiece>();
+
+    closestCube = "None";
+    closestCubeDistance = 99999.0;
+
+    coneNetwork = NetworkTableInstance.getDefault().getTable(tableName);
+    conePieceEntry = cubeNetwork.getEntry("Cones");
+    coneArray = new ArrayList<GamePiece>();
+
+    closestCone = "None";
+    closestConeDistance = 99999.0;
+  }
 
   @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+  public void periodic() {
+    Number[] cubeData = cubePieceEntry.getNumberArray(new Number[0]);
+    cubeArray.clear();
+    int numCube = cubeData.length/4;
+    // Reset search variables for clostest to empty:
+    closestCube = "None";
+    closestCubeDistance = 9999.0;
+
+
+    for (int i = 0; i < numCube; i = i +1){
+      
+      double[] cubes = new double[3];
+
+      cubes[0] = cubeData[i*4 + 1].doubleValue();
+      cubes[1] = cubeData[i*4 + 2].doubleValue();
+      cubes[2] = cubeData[i*4 + 3].doubleValue();
+      cubes[3] = cubeData[i*4 + 4].doubleValue();
+
+      cube.x = cubes[0];
+      cube.y = cubes[1];
+      cube.w = cubes[2];
+      cube.h = cubes[3];
+    }
+
+
+    Number[] coneData = conePieceEntry.getNumberArray(new Number[0]);
+    coneArray.clear();
+    int numCone = coneData.length/4;
+    // Reset search variables for clostest to empty:
+    closestCone = "None";
+    closestConeDistance = 9999.0;
+
+    for (int i = 0; i < numCone; i = i +1){
+      
+      double[] cones = new double[3];
+
+      cones[0] = coneData[i*4 + 1].doubleValue();
+      cones[1] = coneData[i*4 + 2].doubleValue();
+      cones[2] = coneData[i*4 + 3].doubleValue();
+      cones[3] = coneData[i*4 + 4].doubleValue();
+
+      cone.x = cones[0];
+      cone.y = cones[1];
+      cone.w = cones[2];
+      cone.h = cones[3];
+    }
+    //~~~~~~~~~~~~~~~~~~~~ ENTERING CUBE AREA ~~~~~~~~~~~~~~~~~~~~
+    SmartDashboard.putNumber(String.format("%s/NumCube", tableName), numCube);
+    if (closestCube != "None") {
+      SmartDashboard.putString(String.format("%s/ClosestCube", tableName), closestCube);
+      SmartDashboard.putNumber(String.format("%s/ClosestCubeDistance", tableName), closestCubeDistance);
+      SmartDashboard.putNumber(String.format("%s/CubeX", tableName), cube.x);
+      SmartDashboard.putNumber(String.format("%s/CubeY", tableName), cube.y);
+      SmartDashboard.putNumber(String.format("%s/CubeWidth", tableName), cube.w);
+      SmartDashboard.putNumber(String.format("%s/CubeHeight", tableName), cube.h);
+    }
+    else{
+      SmartDashboard.putString(String.format("%s/ClosestCube", tableName), "None");
+      SmartDashboard.putNumber(String.format("%s/ClosestCubeDistance", tableName), 99999.0);
+      SmartDashboard.putNumber(String.format("%s/CubeX", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/CubeY", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/CubeWidth", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/CubeHeight", tableName), 0.0);
+    }
+    //~~~~~~~~~~~~~~~~~~~~ ENTERING CONE AREA ~~~~~~~~~~~~~~~~~~~~
+    SmartDashboard.putNumber(String.format("%s/NumCone", tableName), numCone);
+    if (closestCube != "None") {
+      SmartDashboard.putString(String.format("%s/ClosestCone", tableName), closestCone);
+      SmartDashboard.putNumber(String.format("%s/ClosestConeDistance", tableName), closestConeDistance);
+      SmartDashboard.putNumber(String.format("%s/ConeX", tableName), cone.x);
+      SmartDashboard.putNumber(String.format("%s/ConeY", tableName), cone.y);
+      SmartDashboard.putNumber(String.format("%s/ConeWidth", tableName), cone.w);
+      SmartDashboard.putNumber(String.format("%s/ConeHeight", tableName), cone.h);
+    }
+    else{
+      SmartDashboard.putString(String.format("%s/ClosestCone", tableName), "None");
+      SmartDashboard.putNumber(String.format("%s/ClosestConeDistance", tableName), 99999.0);
+      SmartDashboard.putNumber(String.format("%s/ConeX", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/ConeY", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/ConeWidth", tableName), 0.0);
+      SmartDashboard.putNumber(String.format("%s/ConeHeight", tableName), 0.0);
+    }
+  }
+
+
+  public Pose2d getFieldPoseCube() {
+    if (cubeArray.size() > 0) {
+      //TODO: Implement this method when needed
+      return null; // Until we implement it.
+    }else{
+      return null;
+    }
+  }
+
+  public Pose2d getFieldPoseCone() {
+    if (coneArray.size() > 0) {
+      //TODO: Implement this method when needed
+      return null; // Until we implement it.
+    }else{
+      return null;
+    }
+  }
+
+
+  public String getClosestCube() {
+    return closestCube;
+  }
+
+  public String getClosestCone() {
+    return closestCone;
   }
 }
