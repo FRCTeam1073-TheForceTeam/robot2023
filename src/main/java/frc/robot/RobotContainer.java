8762c1efd454812ea188d753f4dabc1150422560
William <william.vanuitert@gmail.com>
2023-04-02 16:43:20 -0400

    cleaned up RobotContainer
    cleaned up RobotContainer by removing autos we don't use (tested, all autos kept work)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -95,31 +95,23 @@ public class RobotContainer {
   // Auto Chooser
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   private static final String kNoAuto = "No Autonomous";
-  private static final String kBasicEngage = "Basic Engage";
-  private static final String kSplitEngage = "Split Engage";
-  private static final String kEngagePlus = "Engage Plus";
-  private static final String kLeaveCommunity = "Leave Community";
+  //private static final String kBasicEngage = "Basic Engage";
+  //private static final String kSplitEngage = "Split Engage";
+  //private static final String kEngagePlus = "Engage Plus";
+  //private static final String kLeaveCommunity = "Leave Community";
   private static final String kTestMode = "Test Mode";
-  private static final String kAlignToAprilTag = "Align To AprilTag";
-  private static final String kScoreCube = "Score Cube";
-  private static final String kScoreCubeAndEngage = "Score Cube and Engage";
-  private static final String kCubeEngageLeaveCommand = "Score Cube, Leave Community, Engage";
-  private static final String kCubeLeaveCommand = "Score Cube and Leave Community";
-  private static final String kShootHighCubeLeaveEngage = "Shoot Cube, Leave Commmunity, Engage";
-  private static final String kShootHighCubeLeaveBarrierCollect = "Shoot Cube, Barrier Leave, Collect";
-  private static final String kShootHighCubeLeaveCableProtectorCollect = "Shoot Cube, Cable Protector Leave, Collect";
-  private static final String kShootHighCubeLeaveCollectEngage = "Shoot Cube, Leave, Collect, Engage";
+  //private static final String kAlignToAprilTag = "Align To AprilTag";
+  //private static final String kScoreCube = "Score Cube";
+  private static final String kCenterArmCubeEngage = "CenterArmCubeEngage";
+  private static final String kCenterArmCubeLeaveEngage = "CenterArmCubeLeaveEngage";
+  //private static final String kCenterArmCubeLeave = "Score Cube and Leave Community";
+  private static final String kCenterShootCubeLeaveEngage = "CenterShootCubeLeaveEngage";
+  private static final String kBarrierShootCubeLeaveCollectCube = "BarrierShootCubeLeaveCollectCube";
+  private static final String kProtectorShootCubeLeaveCollectCube = "ProtectorShootCubeLeaveCollectCube";
+  private static final String kCenterShootCubeLeaveCollectEngage = "CenterShootCubeLeaveCollectEngage";
 
-  private static final String kArmTest = "Arm Test Command";
-  private static final String kTrajectoryTest = "Drive trajectory Test";
-  // private static final String kScoreHybrid = "Score Hybrid";
-  // private static final String kTrajectoryWaypoint = "Traj Waypoint";
-
-  // private final SendableChooser<String> m_robotLocation = new
-  // SendableChooser<String>();
-  // private static final String kPose1 = "Position 1";
-  // private static final String kPose2 = "Position 2";
-  // private static final String kPose3 = "Position 3";
+  private static final String kArmTest = "ArmTest";
+  private static final String kTrajectoryTest = "DriveTrajectoryTest";
 
   /**
    * Robot container constructor
@@ -134,37 +126,17 @@ public class RobotContainer {
     CommandScheduler.getInstance().setDefaultCommand(m_underglow, m_allianceUnderglow);
 
     m_chooser.setDefaultOption("No Autonomous", kNoAuto);
-    m_chooser.setDefaultOption("Split Engage", kSplitEngage);
-    m_chooser.addOption("Leave (All)", kLeaveCommunity);
-    m_chooser.addOption("Cube and Engage (2)", kScoreCubeAndEngage);
-    m_chooser.addOption("Cube and Leave (All)", kCubeLeaveCommand);
-    m_chooser.addOption("Cube, Leave, and Engage (2)", kCubeEngageLeaveCommand);
-    m_chooser.addOption("1_B_2023_1073", kScoreCube);
-    m_chooser.addOption("2_B_2023_1073", kScoreCube);
-    m_chooser.addOption("3_B_2023_1073", kScoreCube);
-    m_chooser.addOption("2_C_2023_1073", kBasicEngage);
-    m_chooser.addOption("2_D_2023_1073", kEngagePlus);
+    m_chooser.addOption("Center Arm Cube Engage", kCenterArmCubeEngage);
+    m_chooser.addOption("Center Arm Cube Leave Engage", kCenterArmCubeLeaveEngage);
+    m_chooser.addOption("Center Shoot Cube Leave Engage", kCenterShootCubeLeaveEngage);
+    m_chooser.addOption("Barrier Shoot Cube Leave Collect Cube", kBarrierShootCubeLeaveCollectCube);
+    m_chooser.addOption("Protector Shoot Cube Leave Collect Cube", kProtectorShootCubeLeaveCollectCube);
+    m_chooser.addOption("Center Shoot Cube Leave Collect Engage", kCenterShootCubeLeaveCollectEngage);
     m_chooser.addOption("Arm test", kArmTest);
     m_chooser.addOption("Drive Trajectory Test", kTrajectoryTest);
-    m_chooser.addOption("Shoot Cube Leave Engage", kShootHighCubeLeaveEngage);
-    m_chooser.addOption("Shoot Cube, Barrier Leave, Collect", kShootHighCubeLeaveBarrierCollect);
-    m_chooser.addOption("Shoot Cube, Cable Protector Leave, Collect", kShootHighCubeLeaveCableProtectorCollect);
-    m_chooser.addOption("Shoot Cube, Leave, Collect, Engage", kShootHighCubeLeaveCollectEngage);
 
-    // m_chooser.addOption("Test Mode", kTestMode);
-    // m_chooser.addOption("Align To AprilTag", kAlignToAprilTag);
-    // m_chooser.addOption("Arm Set Position test", kArmTest);
-
-    // WEEK 0: commented out superfluous auto choices so DT wouldn't accidentally
-    // choose them
-    // m_chooser.addOption("Score Hybrid", kScoreHybrid);
-    // m_chooser.addOption("Traj Waypoint", kTrajectoryWaypoint);
     SmartDashboard.putData("Auto Chooser", m_chooser);
 
-    // m_robotLocation.setDefaultOption("Position 1", kPose1);
-    // m_robotLocation.addOption("Position 2", kPose2);
-    // m_robotLocation.addOption("Position 3", kPose3);
-    // SmartDashboard.putData("Robot Position Selector", m_robotLocation);
     configureBindings();
     //
   }
@@ -315,44 +287,24 @@ public class RobotContainer {
       case kNoAuto:
         return null;
       // no command ^^^
-      case kBasicEngage:
-        return basicEngage();
-      case kEngagePlus:
-        return engagePlus();
-      // case kEngageExperimental:
-      // return engageExperimental();
-      case kSplitEngage:
-        return splitEngage();
-      case kLeaveCommunity:
-        return leaveCommunity();
       case kTestMode:
         return testMode();
-      // case kScoreHybrid:
-      // return scoreHybrid();
-      // case kTrajectoryWaypoint:
-      // return trajectoryWaypoint();
-      case kAlignToAprilTag:
-        return alignToAprilTag(-0.9, 0.8);
       case kArmTest:
-        return armSetTest();
-      case kScoreCube:
-        return scoreHighCubeCommand();
-      case kScoreCubeAndEngage:
-        return scoreHighCubeAndEngageCommand();
-      case kCubeEngageLeaveCommand:
-        return cubeEngageLeaveCommand();
-      case kCubeLeaveCommand:
-        return cubeLeaveCommand();
+        return armTest();
+      case kCenterArmCubeEngage:
+        return centerArmCubeEngageCommand();
+      case kCenterArmCubeLeaveEngage:
+        return centerArmCubeLeaveEngageCommand();
       case kTrajectoryTest:
-        return testTrajectory();
-      case kShootHighCubeLeaveEngage:
-        return shootHighCubeLeaveEngage();
-      case kShootHighCubeLeaveBarrierCollect:
-        return shootHighCubeLeaveBarrierCollect();
-      case kShootHighCubeLeaveCableProtectorCollect:
-        return shootHighCubeLeaveCableProtectorCollect();
-      case kShootHighCubeLeaveCollectEngage:
-        return shootHighCubeLeaveCollectEngage();
+        return testTrajectoryCommand();
+      case kCenterShootCubeLeaveEngage:
+        return centerShootCubeLeaveEngageCommand();
+      case kBarrierShootCubeLeaveCollectCube:
+        return barrierShootCubeLeaveCollectCube();
+      case kProtectorShootCubeLeaveCollectCube:
+        return protectorShootCubeLeaveCollectCube();
+      case kCenterShootCubeLeaveCollectEngage:
+        return centerShootCubeLeaveCollectEngage();
       default:
         System.out.println("No Auto Selected -_-");
         return null;
@@ -525,36 +477,12 @@ public class RobotContainer {
   }
 
   /**
-   * First step in the sequence to pick a cube off of the ground. Positions arm
-   * above cube to aim before grabbing
-   * 
-   * @return Command that sets arm position
-   */
-
-  /**
-   * Second step in the sequence to pick up a cube off of the ground. Positions
-   * arm so that end effector is up against the cube
-   * 
-   * @return Command that sets arm position
-   */
-
-  /**
-   * An autonomous command to drive up and engage on the charging station using
-   * the Engage command
-   * 
-   * @return Command to do the autonomous outlined above
-   */
-  public Command basicEngage() {
-    return new SequentialCommandGroup(new Engage(m_driveSubsystem, 0.5, false));
-  }
-
-  /**
    * Autonomous command that aligns to april tag, scores a cube in high node, and
    * then engages on the charge station.
    * 
    * @return Command to do the autonomous outlined above
    */
-  public Command scoreHighCubeAndEngageCommand() {
+  public Command centerArmCubeEngageCommand() {
     ArrayList<Arm.JointWaypoints> cubeWaypoints = new ArrayList<Arm.JointWaypoints>();
     cubeWaypoints.add(m_arm.new JointWaypoints(-2.6, 2.8, -1.2, 1.5));
     cubeWaypoints.add(m_arm.new JointWaypoints(-2.0, 3.19, -0.77, 2.25));
@@ -583,7 +511,7 @@ public class RobotContainer {
    * 
    * @return Command to do the autonomous outlined above
    */
-  public Command shootHighCubeLeaveEngage() {
+  public Command centerShootCubeLeaveEngageCommand() {
     ArrayList<Pose2d> communityWaypoints = new ArrayList<Pose2d>();
     communityWaypoints.add(new Pose2d(2.5, 0, new Rotation2d(3.14)));
 
@@ -599,7 +527,7 @@ public class RobotContainer {
             new ParkingBrake(m_driveSubsystem, m_bling, false)));
   }
 
-  public Command shootHighCubeLeaveBarrierCollect() {
+  public Command barrierShootCubeLeaveCollectCube() {
     ArrayList<Pose2d> leaveWaypoints = new ArrayList<Pose2d>();
     if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
       leaveWaypoints.add(new Pose2d(1.3, -.3, new Rotation2d(Math.PI)));
@@ -647,7 +575,7 @@ public class RobotContainer {
         armStowCommand(m_OI));
   }
 
-  public Command shootHighCubeLeaveCableProtectorCollect(){
+  public Command protectorShootCubeLeaveCollectCube(){
     ArrayList<Pose2d> leaveWaypoints = new ArrayList<Pose2d>();
     if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
       leaveWaypoints.add(new Pose2d(1.986, 0.2137, new Rotation2d(Math.PI)));
@@ -698,7 +626,7 @@ public class RobotContainer {
         //TODO: need to speed up so can fit in 15 sec
   }
 
-  public Command shootHighCubeLeaveCollectEngage(){
+  public Command centerShootCubeLeaveCollectEngage(){
     ArrayList<Pose2d> chargeStationWaypoints = new ArrayList<Pose2d>();
     chargeStationWaypoints.add(new Pose2d(1.7579, 0.0116, new Rotation2d(Math.PI)));
 
@@ -744,7 +672,7 @@ public class RobotContainer {
    * 
    * @return Command to do the autonomous outlined above
    */
-  public Command cubeEngageLeaveCommand() {
+  public Command centerArmCubeLeaveEngageCommand() {
 
     ArrayList<Pose2d> communityWaypoints = new ArrayList<Pose2d>();
     communityWaypoints.add(new Pose2d(2.5, 0, new Rotation2d(3.14)));
@@ -773,156 +701,11 @@ public class RobotContainer {
   }
 
   /**
-   * Autonomous command to score a cube, and then leaves the community. Drives
-   * slightly diagonally
-   * to avoid clipping the charging station. Ends up with robot turned 180 degrees
-   * in order to pick up cube
-   * after moving into teleop.
-   * 
-   * @return Command to score cube and leave community
-   */
-  public Command cubeLeaveCommand() {
-
-    ArrayList<Pose2d> communityWaypoints = new ArrayList<Pose2d>();
-    if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
-      communityWaypoints.add(new Pose2d(2.4, 0.15, new Rotation2d(0)));
-    } else {
-      communityWaypoints.add(new Pose2d(2.4, -0.15, new Rotation2d(0)));
-    }
-    ArrayList<Arm.JointWaypoints> cubeWaypoints = new ArrayList<Arm.JointWaypoints>();
-    cubeWaypoints.add(m_arm.new JointWaypoints(-2.6, 2.8, -1.2, 1.5));
-    cubeWaypoints.add(m_arm.new JointWaypoints(-2.0, 3.19, -0.77, 2.25));
-    cubeWaypoints.add(m_arm.new JointWaypoints(-1.483, 3.570, -0.337, 3.0));
-
-    Arm.JointVelocities velocity = m_arm.new JointVelocities(0.6, 0.6, 0.6);
-
-    return new SequentialCommandGroup(
-        new ParallelDeadlineGroup(
-            new ArmSplinePosition(m_arm, cubeWaypoints, velocity, 0.5),
-            new CollectCommand(m_claw, true, 0.8)),
-        new DepositCommand(m_claw, true, 1.0),
-        new ParallelDeadlineGroup(
-            new DriveThroughTrajectory(m_driveSubsystem, communityWaypoints, 1.0, 0.8, 0.5, 0.7),
-            armStowCommand(m_OI)));
-  }
-
-  /**
-   * Autonomous command that aligns to April Tag and activates vacuum in parallel,
-   * extends arm to high node position, deactivates
-   * vacuum, and stows arm.
-   * 
-   * @return Command that scores a cube after aligning to the april tag and then
-   *         stows
-   */
-  public Command scoreHighCubeCommand() {
-    ArrayList<Arm.JointWaypoints> cubeWaypoints = new ArrayList<Arm.JointWaypoints>();
-    cubeWaypoints.add(m_arm.new JointWaypoints(-2.6, 2.8, -1.2, 1.5));
-    cubeWaypoints.add(m_arm.new JointWaypoints(-2.0, 3.19, -0.77, 2.25));
-    cubeWaypoints.add(m_arm.new JointWaypoints(-1.483, 3.570, -0.337, 3.0));
-
-    Arm.JointVelocities velocity = m_arm.new JointVelocities(0.6, 0.6, 0.6);
-
-    return new SequentialCommandGroup(
-        new ParallelDeadlineGroup(
-            new ArmSplinePosition(m_arm, cubeWaypoints, velocity, 0.5),
-            new CollectCommand(m_claw, true, 0.8)),
-        new DepositCommand(m_claw, true, 1.0),
-        armStowCommand(m_OI));
-  }
-
-  public Command cubeLeaveCubeEngage() {
-    ArrayList<Pose2d> communityWaypoints = new ArrayList<Pose2d>();
-    communityWaypoints.add(new Pose2d(4, 0, new Rotation2d(3.14)));
-
-    ArrayList<Arm.JointWaypoints> cubeWaypoints = new ArrayList<Arm.JointWaypoints>();
-    cubeWaypoints.add(m_arm.new JointWaypoints(-2.6, 2.8, -1.2, 1.5));
-    cubeWaypoints.add(m_arm.new JointWaypoints(-2.0, 3.19, -0.77, 2.25));
-    cubeWaypoints.add(m_arm.new JointWaypoints(-1.483, 3.570, -0.337, 3.0));
-
-    ArrayList<Arm.JointWaypoints> cubeGroundWaypoints = new ArrayList<Arm.JointWaypoints>();
-    cubeGroundWaypoints.add(m_arm.new JointWaypoints(-2.6, 2.8, -1.2, 1.5));
-    cubeGroundWaypoints.add(m_arm.new JointWaypoints(-2.0, 3.19, -0.77, 2.25));
-    cubeGroundWaypoints.add(m_arm.new JointWaypoints(-1.483, 3.570, -0.337, 3.0));
-
-    Arm.JointVelocities velocity = m_arm.new JointVelocities(0.6, 0.6, 0.6);
-
-    return new SequentialCommandGroup(
-        new ParallelDeadlineGroup(
-            new ArmSplinePosition(m_arm, cubeWaypoints, velocity, 0.5),
-            new CollectCommand(m_claw, true, 0.8)),
-        new DepositCommand(m_claw, true, 1.0),
-        new ParallelDeadlineGroup(
-            new DriveThroughTrajectory(m_driveSubsystem, communityWaypoints, 0.5,
-                1.0, 0.5, 0.5),
-            armStowCommand(m_OI)));
-  }
-
-  /**
-   * Moves robot back to score a preloaded game piece onto hybrid node, drives
-   * over the charging station to leave community,
-   * then moves backwards onto the charging station and engages using the Engage
-   * command
-   * 
-   * @return Command that scores preload into hybrid node, leaves community, and
-   *         engages
-   */
-  public Command engagePlus() {
-    ArrayList<Pose2d> communityWaypoints = new ArrayList<Pose2d>();
-    ArrayList<Pose2d> scoreWaypoints = new ArrayList<Pose2d>();
-
-    scoreWaypoints.add(new Pose2d(-0.3, 0, new Rotation2d(3.14)));
-    communityWaypoints.add(new Pose2d(2.25, 0, new Rotation2d(3.14)));
-
-    return new SequentialCommandGroup(
-        new DriveThroughTrajectory(m_driveSubsystem, scoreWaypoints, 1.0, 0.8, 0.5, 0.5),
-        new DriveThroughTrajectory(m_driveSubsystem, communityWaypoints, 1.0, 0.8, 0.5, 0.5),
-        new Engage(m_driveSubsystem, 0.5, true));
-    // WEEK 0: changed max velocity in both drive through trajectories to 1.0 from
-    // 0.5, and set engage max speed to 0.5 from 0.3
-  }
-
-  /**
-   * Engages the robot on the charging station using a split up version of the
-   * engage command. First the
-   * robot drives up to charging station, then drives up the charging station,
-   * then goes to balance, and then parks
-   * 
-   * @return a command to engage the robot on the charging station.
-   */
-  public Command splitEngage() {
-    return new SequentialCommandGroup(
-        new EngageDriveUp(m_driveSubsystem, Preferences.getDouble("EngageDriveUp.maxSpeed", 0.9), false),
-        new EngageForward(m_driveSubsystem, Preferences.getDouble("EngageForawrd.maxSpeed", 0.7), false),
-        new EngageBalance(m_driveSubsystem, Preferences.getDouble("EngageBalance.maxSpeed", 0.7), false),
-        new ParkingBrake(m_driveSubsystem, m_bling, false));
-  }
-
-  /**
-   * Autonomous command to leave the community. Uses DrivethroughTrajectory to
-   * carry robot out of community.
-   * 
-   * @return a sequential command group containing a DrivethroughTrajectory
-   *         command
-   */
-  public Command leaveCommunity() {
-
-    System.out.println("Leave Community");
-
-    ArrayList<Pose2d> waypoints = new ArrayList<Pose2d>();
-    waypoints.add(new Pose2d(2.5, 0.0, new Rotation2d(3.14)));
-    waypoints.add(new Pose2d(2.5, -1.0, new Rotation2d(3.14)));
-    waypoints.add(new Pose2d(2.5, -1, new Rotation2d(0)));
-
-    return new SequentialCommandGroup(new DriveThroughTrajectory(m_driveSubsystem, waypoints, 1, 1, 0.5, 0.5));
-
-  }
-
-  /**
    * Tests ArmSetPosition
    * 
    * @return an ArmSetPosition Command
    */
-  public Command armSetTest() {
+  public Command armTest() {
     ArrayList<Arm.JointWaypoints> waypoints = new ArrayList<Arm.JointWaypoints>();
     waypoints.add(m_arm.new JointWaypoints(-1.0, 2.6, 0.0, 6.0));
     waypoints.add(m_arm.new JointWaypoints(-1.55, 2.4, 0.0, 9.0));
@@ -931,7 +714,7 @@ public class RobotContainer {
     return new ArmSplinePosition(m_arm, waypoints, velocity, 0.5);
   }
 
-  public Command testTrajectory() {
+  public Command testTrajectoryCommand() {
     ArrayList<Pose2d> waypoints = new ArrayList<Pose2d>();
     waypoints.add(new Pose2d(0.5, 0, new Rotation2d(3.14)));
     waypoints.add(new Pose2d(0.5, 0.5, new Rotation2d(3.14)));
@@ -955,40 +738,6 @@ public class RobotContainer {
     System.out.println("Test Mode on");
     return new DriveTestCommand(m_driveSubsystem, m_OI);
   }
-
-  // public Command scoreHybrid() {
-  // System.out.println("Hybrid Scored");
-  // return null;
-  // }
-
-  // public Command trajectoryWaypoint() {
-  // System.out.println("Waypoint Beginning");
-
-  // ArrayList<Pose2d> waypoints = new ArrayList<Pose2d>();
-
-  // FORWARD TEST
-
-  // waypoints.add(new Pose2d(0.0, 0.0, new Rotation2d()));
-  // waypoints.add(new Pose2d(0.5, 0.0, new Rotation2d()));
-  // waypoints.add(new Pose2d(1.0, 0.0, new Rotation2d()));
-  // waypoints.add(new Pose2d(1.0, 0.0, new Rotation2d()));
-
-  // BOX TEST
-  // waypoints.add(new Pose2d(0.0, 0.0, new Rotation2d()));
-  // waypoints.add(new Pose2d(1.0, 0.0, new Rotation2d()));
-  // waypoints.add(new Pose2d(1.0, -1.0, new Rotation2d()));
-  // waypoints.add(new Pose2d(0.0, -1.0, new Rotation2d()));
-
-  // waypoints.add(new Pose2d(0.0, 0.0, new Rotation2d(3.1)));
-  // return new SequentialCommandGroup(
-  // new DriveThroughTrajectory(m_driveSubsystem, new Pose2d(0,0, new
-  // Rotation2d()), waypoints, 0.5, 0.8, 0.5, 0.5));
-
-  // return new SequentialCommandGroup(new
-  // DriveThroughTrajectory(m_driveSubsystem, new Pose2d(0,0,
-  // new Rotation2d()), waypoints, 0.5, 0.8, 0.5, 0.5));
-  // }
-
   /**
    * Uses AlignToAprilTag to align to an AprilTag
    * 
